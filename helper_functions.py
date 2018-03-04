78fe305903e55e927e313a433e9bd267b50d0fc8
Kar Yin Ng <karyin3@uw.edu>
2018-03-03 16:08:47 -0800

    filter business categories

---
+++ helper_functions.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns # for visualiation
+import matplotlib.pyplot as plt # plotting
+import matplotlib
+
+business_data = pd.read_csv('./data/yelp_business.csv')
+tip_data = pd.read_csv('./data/yelp_tip.csv')
+
+def filter_category():
+    new_df = business_data[['business_id', 'city', 'state', 'stars', 'categories']]
+    return new_df[new_df['categories'].str.contains('Cafes|Coffee & Tea')]
