51589e8a1fa8c344c3736592784e384b5bb773e4
Kar Yin Ng <karyin3@uw.edu>
2018-03-03 21:50:50 -0800

    add comments to helper functions

--- helper_functions.py
+++ helper_functions.py
@@ -9,23 +9,27 @@ from nltk.tokenize import word_tokenize
 from collections import Counter
 from textblob import TextBlob
 
-# load data
+# Load data
 business_data = pd.read_csv('./data/yelp_business.csv')
 tip_data = pd.read_csv('./data/yelp_tip.csv')
 
+# Select businesses under 'Cafes' and 'Coffee & Tea' categories
 def filter_category():
     df = business_data[['business_id', 'city', 'state', 'stars', 'categories']]
     return df[df['categories'].str.contains('Cafes|Coffee & Tea')]
 
+# Merge business ids (with text of tips) from tips dataframe
 def merge_id():
     df = filter_category()
     return pd.merge(df, tip_data[['business_id','text']], on='business_id')
 
+# Read sentiment words line by line from .txt file
 def readwords(filename):
     f = open(filename)
     words = [line.rstrip() for line in f.readlines()]
     return words
 
+# Process string to remove punctuation, stopwords and numerical characters
 def process_string(s):
     string = re.split("\r\n", s)
     result = ""
@@ -36,6 +40,7 @@ def process_string(s):
     remove_stopwords = [word for word in word_tokens if word not in stop_words]
     return [word for word in remove_stopwords if word.isalpha()]
 
+# Count the number of positive words in a string
 def count_positive(row):
     text = row['text']
     positive = readwords('./positive-words.txt')
@@ -48,6 +53,7 @@ def count_positive(row):
             pos += val
     return pos
 
+# Count the number of negative words in a string
 def count_negative(row):
     text = row['text']
     negative = readwords('./negative-words.txt')
@@ -60,11 +66,16 @@ def count_negative(row):
             neg += val
     return neg
 
+# Returns the overall sentiment (polarity score) of a string
+# Polarity score is a float within the range -1.0 and 1.0
 def overall_sentiment(row):
     text = row['text']
     tips = TextBlob(text)
     return round(tips.sentiment.polarity, 2)
 
+# Returns a dataframe which consists columns such as:
+# 'business_id', 'city', 'state', 'stars', 'categories', 'text',
+# 'num_positive_words', 'num_negative_words', 'overall_sentiment'
 def get_final_df():
     df = merge_id()
     df['num_positive_words'] = df.apply(count_positive, axis=1)
