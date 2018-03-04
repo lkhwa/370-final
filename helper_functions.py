233f2242f50eb104c17c3f5a85bc7efa7b3f88a8
Kar Yin Ng <karyin3@uw.edu>
2018-03-03 16:48:08 -0800

    merge business IDs from tips csv

--- helper_functions.py
+++ helper_functions.py
@@ -10,3 +10,7 @@ tip_data = pd.read_csv('./data/yelp_tip.csv')
 def filter_category():
     new_df = business_data[['business_id', 'city', 'state', 'stars', 'categories']]
     return new_df[new_df['categories'].str.contains('Cafes|Coffee & Tea')]
+
+def merge_id():
+    df = filter_category()
+    return pd.merge(df, tip_data[['business_id','text']], on='business_id')
