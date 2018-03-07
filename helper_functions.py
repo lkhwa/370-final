94bbab2cc23ab32461aafe5413b06ed94f7771c1
Kar Yin Ng <karyin3@uw.edu>
2018-03-07 15:01:43 -0800

    remove unused functions and reorganized code on notebook

--- helper_functions.py
+++ helper_functions.py
@@ -24,12 +24,6 @@ def merge_id():
     df = filter_category()
     return pd.merge(df, tip_data[['business_id','text']], on='business_id')
 
-# Read sentiment words line by line from .txt file
-def readwords(filename):
-    f = open(filename)
-    words = [line.rstrip() for line in f.readlines()]
-    return words
-
 # Process string to remove punctuation, stopwords and numerical characters
 def process_string(s):
     string = re.split("\r\n", s)
@@ -41,59 +35,14 @@ def process_string(s):
     remove_stopwords = [word for word in word_tokens if word not in stop_words]
     return [word for word in remove_stopwords if word.isalpha()]
 
-# Count the number of positive words in a string
-def count_positive(row):
-    text = row['text']
-    positive = readwords('./positive-words.txt')
-    string = process_string(text)
-    count = Counter(string)
-
-    pos = 0
-    for key, val in count.items():
-        if key in positive:
-            pos += val
-    return pos
-
-# Count the number of negative words in a string
-def count_negative(row):
-    text = row['text']
-    negative = readwords('./negative-words.txt')
-    string = process_string(text)
-    count = Counter(string)
-
-    neg = 0
-    for key, val in count.items():
-        if key in negative:
-            neg += val
-    return neg
-
-# Returns the overall sentiment (polarity score) of a string
-# Polarity score is a float within the range -1.0 and 1.0
-def overall_sentiment(row):
-    text = row['text']
-    tips = TextBlob(text)
-    return round(tips.sentiment.polarity, 2)
-
-# Returns a dataframe which consists columns such as:
-# 'business_id', 'city', 'state', 'stars', 'categories', 'text',
-# 'num_positive_words', 'num_negative_words', 'overall_sentiment'
-def get_final_df():
-    df = merge_id()
-    df['num_positive_words'] = df.apply(count_positive, axis=1)
-    df['num_negative_words'] = df.apply(count_negative, axis=1)
-    df['overall_sentiment'] = df.apply(overall_sentiment, axis=1)
-    return df
-
-def aggregate_df(df):
-    df1 = df[['business_id', 'stars', 'num_positive_words', 'num_negative_words', 'overall_sentiment']]
-    df1 = df1.groupby('business_id').agg({'stars': 'first', 'num_positive_words':'sum', 'num_negative_words': 'sum', 'overall_sentiment': 'sum'}).reset_index()
-    return df1
-
+# Returns 100 most frequently used words across all tips
 def top_hundred(df):
     string = (' '.join(df['text']))
     fdist = FreqDist(process_string(string))
     return fdist.most_common(100)
 
+# This function is for tip analysis purpose
+# For each word in the list, get an example of tip (which contains the word) for each level of star rating
 def get_tips(list, data):
     pd.set_option('display.max_colwidth', -1)
     new = pd.DataFrame()
@@ -104,11 +53,16 @@ def get_tips(list, data):
         new = new.append(df1, ignore_index=True)
     return new
 
+# Returns a dataframe which contains the top 100 businesses in terms of number of tips received
+# Dataframe also contains the text of all the tips for a business, rating,
+# and the frequency of occurences of each word in tips (based on a predefined list of words)
 def top_businesses_by_tip(data, word_list):
     pd.set_option('max_colwidth',40)
     df1 = data[['business_id', 'text','state', 'stars']]
     df1 = df1.groupby('business_id').agg({'state': len, 'text': lambda x: ' '.join(x), 'stars': 'mean'}).reset_index().sort_values(by=['state'], ascending=False)
     df1.rename(columns={'state': 'tip_count', 'stars': 'rating'}, inplace=True)
+    
+    # Count the number of times each word appears in all tips
     for word in word_list:
         df1[word] = df1['text'].apply(lambda text: text.count(word))
     return df1.head(n=100)
